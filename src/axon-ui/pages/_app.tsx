360ce29fc2a712f7172eff987f4fd26c606a59ca
Norton Wang <1502552+FloorLamp@users.noreply.github.com>
2021-07-16 15:55:32 +0700

    Basic UI

---
+++ src/axon-ui/pages/_app.tsx
@@ -0,0 +1,8 @@
+import "react";
+import "tailwindcss/tailwind.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
--- src/declarations/Axon/index.js
+++ src/declarations/Axon/index.js
@@ -1,21 +1,23 @@
 import { Actor, HttpAgent } from "@dfinity/agent";
 
 // Imports candid interface
-import { idlFactory } from './Axon.did.js';
-// CANISTER_ID is replaced by webpack based on node enviroment
-export const canisterId = process.env.AXON_CANISTER_ID;
+import { idlFactory } from "./Axon.did.js";
+
+import CanisterIds from "../../../canister_ids.json";
+
+export const canisterId = CanisterIds.Axon.ic;
 
 /**
- * 
+ *
  * @param {string | Principal} canisterId Canister ID of Agent
  * @param {{agentOptions?: import("@dfinity/agent").HttpAgentOptions; actorOptions?: import("@dfinity/agent").ActorConfig}} [options]
  * @return {import("@dfinity/agent").ActorSubclass<import("./Axon.did.js")._SERVICE>}
  */
- export const createActor = (canisterId, options) => {
+export const createActor = (canisterId, options) => {
   const agent = new HttpAgent({ ...options?.agentOptions });
-  
+
   // Fetch root key for certificate validation during development
-  if(process.env.NODE_ENV !== "production") agent.fetchRootKey();
+  if (process.env.NODE_ENV !== "production") agent.fetchRootKey();
 
   // Creates an actor with using the candid interface and the HttpAgent
   return Actor.createActor(idlFactory, {
@@ -24,9 +26,3 @@ export const canisterId = process.env.AXON_CANISTER_ID;
     ...options?.actorOptions,
   });
 };
-  
-/**
- * A ready-to-use agent for the Axon canister
- * @type {import("@dfinity/agent").ActorSubclass<import("./Axon.did.js")._SERVICE>}
- */
- export const Axon = createActor(canisterId);
